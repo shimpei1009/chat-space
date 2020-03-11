4ec5f49635eaf470d7c51e0dbd6ba1f5a063f5f5
shimpei1009 <gogo.ikeike.1009@icloud.com>
2020-03-11 21:47:35 +0900

    search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  
+  def index
+    respond_to do |format|
+      @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
